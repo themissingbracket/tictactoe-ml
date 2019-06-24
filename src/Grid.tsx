94d08fd87988a1525d6656eab07501add81c931e
Rahul <k.dasgupta112@gmail.com>
2019-06-24 22:18:20 +0800

    Figured out the game check

--- src/Grid.tsx
+++ src/Grid.tsx
@@ -1,4 +1,4 @@
-import React,{ FC, useState } from 'react';
+import React,{ FC, useState,Fragment } from 'react';
 
 
 
@@ -23,6 +23,12 @@ const Grid:FC = (props)=>{
   const [ Blocks, setBlock ] = useState(Array(gridCount).fill(null))
   const [isPlayerX,setisPlayerX] = useState(true)
   const [ gameOver,setgameOver ] = useState(false)
+
+  const Reset = ()=>{
+    setBlock(Array(gridCount).fill(null))
+    setisPlayerX(true)
+    setgameOver(false)
+  }
   const blockClickedController=(position:number):void=>{
       
       const newBlocks = Blocks.map((i:any,idx:number)=>{
@@ -34,43 +40,74 @@ const Grid:FC = (props)=>{
       setisPlayerX(newPlayer)
       // console.log(newBlocks)
       // setBlock(newBlocks)
-    if (!newBlocks.find(value => null)) CheckGameState(newBlocks)
+    // if (!newBlocks.find(value => null)) 
+    CheckGameState(newBlocks)
     
   }
-  const CheckGameState=(blocks:Object):void=>{
-      console.log(blocks)
+  const CheckGameState=(blocks:string[]):void=>{
+    
+    const isEqual = (array:string[]):boolean => (array&&array.length===3)?array.every((a:string):boolean=>a===array[0]):false
+    
+    const winner = (
+      /**
+       * @Columns
+       */
+      isEqual(blocks.filter((value, idx) => ([0, 3, 6].includes(idx))&& value )) ||//?
+      isEqual(blocks.filter((value, idx) => ([1, 4, 7].includes(idx))&& value )) ||//?
+      isEqual(blocks.filter((value, idx) => ([2, 5, 8].includes(idx)) && value)) ||//?
+      /**
+       * @Rows
+       */
+      isEqual(blocks.filter((value, idx) => ([0, 1, 2].includes(idx)) && value)) ||//?
+      isEqual(blocks.filter((value, idx) => ([3, 4, 5].includes(idx)) && value)) ||//?
+      isEqual(blocks.filter((value, idx) => ([6, 7, 8].includes(idx)) && value)) ||//?
+      /**
+       * @Diagonals
+       */
+      isEqual(blocks.filter((value, idx) => ([0, 4, 8].includes(idx))&& value )) ||//?
+      isEqual(blocks.filter((value, idx) => ([2, 4, 6].includes(idx))&& value ))
+    )
+    console.log(winner)
+    if (winner) return setgameOver(true)
+    // blocks.filter(val => val === null).length?console.log(true):console.log(false)
+    if (!winner && blocks.find(val => val === null)===undefined) return setgameOver(true)
+      
   }
+  const Message = gameOver ? 
+  (Blocks.find(val => val === null) === undefined)?'Its a draw!!!'
+        :
+      `Player ${!isPlayerX ? "X" : "O"} wins!!!`
+      : 
+   `Curent Player : ${isPlayerX ? "X" : "O"}`
   // Blocks.map(i=>console.log(i))
   return (
-    
-    <div className="grid">
-        
-        <header>
-          {
-            gameOver?
-              "GameOver"
-            :
-            `Curent Player : ${ isPlayerX?"X":"O"}`
-          }
-          
-        </header>
-
-        <div className="row" >
-          <Block value={Blocks[0]} onClick={!gameOver?blockClickedController:null} position={0} />
-          <Block value={Blocks[1]} onClick={!gameOver?blockClickedController:null} position={1} />
-          <Block value={Blocks[2]} onClick={!gameOver?blockClickedController:null} position={2} />
-        </div>
-        <div className="row" >
-          <Block value={Blocks[3]} onClick={!gameOver?blockClickedController:null} position={3} />
-          <Block value={Blocks[4]} onClick={!gameOver?blockClickedController:null} position={4} />
-          <Block value={Blocks[5]} onClick={!gameOver?blockClickedController:null} position={5} />
-        </div>
-        <div className="row" >
-          <Block value={Blocks[6]} onClick={!gameOver?blockClickedController:null} position={6} />
-          <Block value={Blocks[7]} onClick={!gameOver?blockClickedController:null} position={7} />
-          <Block value={Blocks[8]} onClick={!gameOver?blockClickedController:null} position={8} />
-        </div>
-      </div>
+    <Fragment>
+        <div className="grid">
+            <header>
+              {
+                Message
+              }
+              
+            </header>
+            <div className="row" >
+              <Block value={Blocks[0]} onClick={!gameOver?blockClickedController:null} position={0} />
+              <Block value={Blocks[1]} onClick={!gameOver?blockClickedController:null} position={1} />
+              <Block value={Blocks[2]} onClick={!gameOver?blockClickedController:null} position={2} />
+            </div>
+            <div className="row" >
+              <Block value={Blocks[3]} onClick={!gameOver?blockClickedController:null} position={3} />
+              <Block value={Blocks[4]} onClick={!gameOver?blockClickedController:null} position={4} />
+              <Block value={Blocks[5]} onClick={!gameOver?blockClickedController:null} position={5} />
+            </div>
+            <div className="row" >
+              <Block value={Blocks[6]} onClick={!gameOver?blockClickedController:null} position={6} />
+              <Block value={Blocks[7]} onClick={!gameOver?blockClickedController:null} position={7} />
+              <Block value={Blocks[8]} onClick={!gameOver?blockClickedController:null} position={8} />
+            </div>
+            
+          </div>
+          <button onClick={Reset}> Reset Game </button>
+    </Fragment>
   )
 }
 
