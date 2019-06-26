2733a0a818eab6acf45c1b4276c3c98bcd4be44a
Rahul <k.dasgupta112@gmail.com>
2019-06-26 20:49:08 +0800

    Game mechaninc built

--- src/Grid.tsx
+++ src/Grid.tsx
@@ -1,18 +1,19 @@
-import React,{ FC, useState,Fragment } from 'react';
+import React, { FC, useState, Fragment } from 'react';
 
 
 
 interface BlockProps{
   position:number,
   onClick:any,
-  value:string
+  value:string,
+  winning:boolean
 }
 
-const Block: FC<BlockProps> = ({ value,position,onClick})=>{
+const Block: FC<BlockProps> = ({ value,position,onClick,winning})=>{
   
   
   return (
-    <div className={`block ${!onClick?"disabled":""}`} onClick={()=>onClick(position)}>
+    <div className={`block ${value?"disabled":""} ${winning?'winning':''}`} onClick={()=>onClick(position)}>
       <span>{value}</span>
     </div>
   )
@@ -23,11 +24,15 @@ const Grid:FC = (props)=>{
   const [ Blocks, setBlock ] = useState(Array(gridCount).fill(null))
   const [isPlayerX,setisPlayerX] = useState(true)
   const [ gameOver,setgameOver ] = useState(false)
-
+  const [winningHand,setWinningHand] = useState([] as number[])
+  // useEffect(() => console.log(Blocks))
+  // const [ winninHand , setwinningHand ] = useState([])
   const Reset = ()=>{
     setBlock(Array(gridCount).fill(null))
     setisPlayerX(true)
     setgameOver(false)
+    const winningHand:number[] = [] 
+    setWinningHand(winningHand)
   }
   const blockClickedController=(position:number):void=>{
       
@@ -36,8 +41,7 @@ const Grid:FC = (props)=>{
         return i
       })
       setBlock(newBlocks)
-      const newPlayer = !isPlayerX
-      setisPlayerX(newPlayer)
+      
       // console.log(newBlocks)
       // setBlock(newBlocks)
     // if (!newBlocks.find(value => null)) 
@@ -45,41 +49,57 @@ const Grid:FC = (props)=>{
     
   }
   const CheckGameState=(blocks:string[]):void=>{
+
+    const sequence:number[][] = [
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ]
+    //Checks the Array only if there are three elements
+    // This is to prevent the true value when the first move is made
+    const isEqual:Function = (array:string[]):boolean => (array&&array.length===3)? array.every((a:string):boolean => a === array[0]) : false
     
-    const isEqual = (array:string[]):boolean => (array&&array.length===3)?array.every((a:string):boolean=>a===array[0]):false
+    /**
+     * TODO: There has to be better way of doing these checks
+     * Dont really like hardcoding the index values like that
+     */
+
+     const mappedSeq:string[][] = sequence.map(seq => blocks.filter((value,idx)=>(seq.includes(idx) && value)))
+
+     const winner:boolean = mappedSeq.some(value => isEqual(value))
+
+     const winningHand:string[]|undefined = mappedSeq.find(val=>isEqual(val))
+     
+
+    // let winningSeq = sequence[ mappedSeq.indexOf(mappedSeq.find(val=>isEqual(val))) ]
+
+    // const winner:boolean = sequence.map( ( seq:number[] ) => isEqual(blocks.filter((b:string,idx:number) => seq.includes(idx) && b))).some( (val:boolean) => val )
     
-    const winner = (
-      /**
-       * @Columns
-       */
-      isEqual(blocks.filter((value, idx) => ([0, 3, 6].includes(idx))&& value )) ||//?
-      isEqual(blocks.filter((value, idx) => ([1, 4, 7].includes(idx))&& value )) ||//?
-      isEqual(blocks.filter((value, idx) => ([2, 5, 8].includes(idx)) && value)) ||//?
-      /**
-       * @Rows
-       */
-      isEqual(blocks.filter((value, idx) => ([0, 1, 2].includes(idx)) && value)) ||//?
-      isEqual(blocks.filter((value, idx) => ([3, 4, 5].includes(idx)) && value)) ||//?
-      isEqual(blocks.filter((value, idx) => ([6, 7, 8].includes(idx)) && value)) ||//?
-      /**
-       * @Diagonals
-       */
-      isEqual(blocks.filter((value, idx) => ([0, 4, 8].includes(idx))&& value )) ||//?
-      isEqual(blocks.filter((value, idx) => ([2, 4, 6].includes(idx))&& value ))
-    )
-    console.log(winner)
+    if(winningHand){
+      const winningSequence:number[] = sequence[mappedSeq.indexOf(winningHand)]
+      setWinningHand(winningSequence)
+    }
     if (winner) return setgameOver(true)
-    // blocks.filter(val => val === null).length?console.log(true):console.log(false)
+
     if (!winner && blocks.find(val => val === null)===undefined) return setgameOver(true)
+    const newPlayer = !isPlayerX
+    setisPlayerX(newPlayer)
       
   }
+  // console.log(winningHand.length>0?`winning length : ${winningHand.length}`:`Nope`)
   const Message = gameOver ? 
-  (Blocks.find(val => val === null) === undefined)?'Its a draw!!!'
+    (winningHand.length === 0) ? `It's a draw!!`
         :
-      `Player ${!isPlayerX ? "X" : "O"} wins!!!`
+      `Player ${isPlayerX ? "X" : "O"} wins!!!`
       : 
    `Curent Player : ${isPlayerX ? "X" : "O"}`
-  // Blocks.map(i=>console.log(i))
+   // Blocks.map(i=>console.log(i))
+  //  console.log(Blocks)
   return (
     <Fragment>
         <div className="grid">
@@ -90,19 +110,19 @@ const Grid:FC = (props)=>{
               
             </header>
             <div className="row" >
-              <Block value={Blocks[0]} onClick={!gameOver?blockClickedController:null} position={0} />
-              <Block value={Blocks[1]} onClick={!gameOver?blockClickedController:null} position={1} />
-              <Block value={Blocks[2]} onClick={!gameOver?blockClickedController:null} position={2} />
+              <Block winning={winningHand.includes(0)?true:false} value={Blocks[0]} onClick={!gameOver?blockClickedController:null} position={0} />
+              <Block winning={winningHand.includes(1)?true:false} value={Blocks[1]} onClick={!gameOver?blockClickedController:null} position={1} />
+              <Block winning={winningHand.includes(2)?true:false} value={Blocks[2]} onClick={!gameOver?blockClickedController:null} position={2} />
             </div>
             <div className="row" >
-              <Block value={Blocks[3]} onClick={!gameOver?blockClickedController:null} position={3} />
-              <Block value={Blocks[4]} onClick={!gameOver?blockClickedController:null} position={4} />
-              <Block value={Blocks[5]} onClick={!gameOver?blockClickedController:null} position={5} />
+              <Block winning={winningHand.includes(3)?true:false} value={Blocks[3]} onClick={!gameOver?blockClickedController:null} position={3} />
+              <Block winning={winningHand.includes(4)?true:false} value={Blocks[4]} onClick={!gameOver?blockClickedController:null} position={4} />
+              <Block winning={winningHand.includes(5)?true:false} value={Blocks[5]} onClick={!gameOver?blockClickedController:null} position={5} />
             </div>
             <div className="row" >
-              <Block value={Blocks[6]} onClick={!gameOver?blockClickedController:null} position={6} />
-              <Block value={Blocks[7]} onClick={!gameOver?blockClickedController:null} position={7} />
-              <Block value={Blocks[8]} onClick={!gameOver?blockClickedController:null} position={8} />
+              <Block winning={winningHand.includes(6)?true:false} value={Blocks[6]} onClick={!gameOver?blockClickedController:null} position={6} />
+              <Block winning={winningHand.includes(7)?true:false} value={Blocks[7]} onClick={!gameOver?blockClickedController:null} position={7} />
+              <Block winning={winningHand.includes(8)?true:false} value={Blocks[8]} onClick={!gameOver?blockClickedController:null} position={8} />
             </div>
             
           </div>
